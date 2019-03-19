b8b3becffce6766707e5f30d107a6dc9bc47c46d
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-03-19 21:48:33 +0800

    rewrite the vue
    rewrite the vue

--- ebook-vue/src/router/index.js
+++ ebook-vue/src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Index from '@/components/Index'
-import SignUp from '@/components/SignUp'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Index',
-      component: Index
-    },
-    {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUp
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
