741675e8a5cc93afbf4943183bd9cc3673792eb3
Bert-Z <wanqiangzhang@sjtu.edu.cn>
2019-04-17 23:24:27 +0800

    finish indexnav

--- BackEnd/src/main/java/top/bertz/controller/ApiController.java
+++ BackEnd/src/main/java/top/bertz/controller/ApiController.java
@@ -9,6 +9,7 @@ import top.bertz.repository.CategoryRepository;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = {"/api"})
@@ -16,8 +17,13 @@ public class ApiController {
     @Autowired
     CategoryRepository categoryRepo;
 
-    @RequestMapping(value = {"/getCategories"}, produces = "application/json;charset=UTF-8")
-    public Iterable<Categorys> getCategories(HttpServletResponse response) {
+    @RequestMapping(value = {"/getCategory2"}, produces = "application/json;charset=UTF-8")
+    public List<Categorys> getCategory2(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        return categoryRepo.findCategory2();
+    }
+    @RequestMapping(value = {"/getAllCategorys"}, produces = "application/json;charset=UTF-8")
+    public Iterable<Categorys> getAllCaregorys(HttpServletResponse response){
         response.addHeader("Access-Control-Allow-Origin", "*");
         return categoryRepo.findAll();
     }
--- BackEnd/src/main/java/top/bertz/repository/CategoryRepository.java
+++ BackEnd/src/main/java/top/bertz/repository/CategoryRepository.java
@@ -1,8 +1,13 @@
 package top.bertz.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import top.bertz.entity.Categorys;
 
+import java.util.List;
+
 public interface CategoryRepository extends CrudRepository<Categorys, Integer> {
+    @Query("select distinct category2 from Categorys")
+    public List<Categorys> findCategory2();
 
 }
